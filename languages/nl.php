8f5c2f0e7497f6b7e212ac1411898e3aa6668c82
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-17 09:57:58 +0200

    dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-07-17 09:57
+ */
+
+$language = array (
+  'quicklinks:owner:title' => '%s\'s links',
+  'quicklinks:edit:url' => 'Website URL',
+  'quicklinks' => 'Mijn links',
+  'quicklinks:add' => 'Link toevoegen',
+  'quicklinks:menu:entity:title' => 'Toon in mijn links',
+  'quicklinks:widget:title' => 'Mijn links',
+  'quicklinks:widget:description' => 'Toont al je favoriete links',
+  'quicklinks:add:title' => 'Link toevoegen',
+  'quicklinks:edit:required' => 'Velden met een * zijn verplicht',
+  'quicklinks:action:toggle:success:add' => 'Toegevoegd aan mijn links',
+  'quicklinks:action:toggle:success:remove' => 'Verwijderd van mijn links',
+  'quicklinks:action:delete:error' => 'Er is een fout opgetreden tijdens het verwijderen van de link',
+  'quicklinks:action:delete:success' => 'Link verwijderd',
+);
+add_translation("nl", $language);
