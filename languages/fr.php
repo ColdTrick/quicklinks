35979decd8216ba87252f795e5f346991fb99fd4
bm jade <benoit.mottin@jade.fr>
2017-04-06 10:37:38 +0200

    I18N   add french translation

---
+++ languages/fr.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+	'quicklinks' => "Liens rapides",
+	'quicklinks:add' => "Ajouter un lien rapide",
+	'quicklinks:move' => "Déplacer",
+	'item:object:quicklink' => 'Liens rapides',
+	
+	// menu's
+	'quicklinks:menu:entity:title' => "Basculer vers le lien rapide",
+	
+	// widgets
+	'quicklinks:widget:title' => "Liens rapides",
+	'quicklinks:widget:description' => "Affiche une liste des liens rapides que vous avez fait",
+	
+	// views
+	'quicklinks:owner:title' => "Liens rapides de %s",
+	
+	'quicklinks:add:title' => "Ajouter un lien rapide",
+	'quicklinks:edit:url' => "URL du site web",
+	'quicklinks:edit:required' => "Les champs marqués d'une * sont obligatoires",
+	
+	// actions
+	'quicklinks:action:toggle:success:add' => "Lien rapide ajouté",
+	'quicklinks:action:toggle:success:remove' => "Lien rapide supprimé",
+	
+	'quicklinks:action:delete:error' => "Une erreur est survenue lors de la suppression de votre lien rapide",
+	'quicklinks:action:delete:success' => "Votre lien rapide a été supprimé",
+	'' => "",
+);
