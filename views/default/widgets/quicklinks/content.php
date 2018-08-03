d0ada4b3dba20e00a4966b79a7031ffdeea43abe
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-08-03 09:15:37 +0200

    chore: add quicklink from widget footer link

--- views/default/widgets/quicklinks/content.php
+++ views/default/widgets/quicklinks/content.php
@@ -25,3 +25,13 @@ if (empty($menu)) {
 }
 
 echo $menu;
+
+if ($owner->guid === elgg_get_logged_in_user_guid()) {
+	$add = elgg_view('output/url', [
+		'icon' => 'plus',
+		'text' => elgg_echo('quicklinks:add'),
+		'href' => elgg_generate_url('add:object:quicklink', ['guid' => elgg_get_logged_in_user_guid()]),
+	]);
+	
+	echo elgg_format_element('div', ['class' => 'elgg-widget-more'], $add);
+}
