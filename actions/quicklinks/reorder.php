932e1c32b614c72b5b76b7a127c007c794ae120f
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-10 16:17:18 +0100

    chore: replaced deprecated function

--- actions/quicklinks/reorder.php
+++ actions/quicklinks/reorder.php
@@ -3,6 +3,7 @@
 $container_guid = (int) get_input('container_guid', elgg_get_logged_in_user_guid());
 $ordered_guids = get_input('guids', '');
 
-if (get_entity($container_guid)) {
-	set_private_setting($container_guid, 'quicklinks_order', json_encode($ordered_guids));
+$container = get_entity($container_guid);
+if (!empty($container)) {
+	$container->setPrivateSetting('quicklinks_order', json_encode($ordered_guids));
 }
