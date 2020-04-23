21ec58999554eaffd12c83d1e48c0ab64539ad97
Jerome Bakker <jeabakker@coldtrick.com>
2020-04-23 16:39:20 +0200

    chore: no longer use deprecated layout

--- views/default/resources/quicklinks/add.php
+++ views/default/resources/quicklinks/add.php
@@ -3,12 +3,11 @@
  * add a new QuickLink
  */
 
-elgg_gatekeeper();
+use Elgg\EntityNotFoundException;
 
 $page_owner = elgg_get_page_owner_entity();
-if (!($page_owner instanceof ElggUser)) {
-	register_error(elgg_echo('pageownerunavailable', [elgg_get_page_owner_guid()]));
-	forward(REFERER);
+if (!$page_owner instanceof ElggUser) {
+	throw new EntityNotFoundException(elgg_echo('pageownerunavailable', [elgg_get_page_owner_guid()]));
 }
 
 // build page elements
@@ -16,16 +15,14 @@ $title_text = elgg_echo('quicklinks:add:title');
 
 $content = elgg_view_form('quicklinks/edit', [], ['container' => $page_owner]);
 
+// draw page
 if (elgg_is_xhr()) {
+	// light version for lightbox
 	echo elgg_view_module('inline', $title_text, $content);
-} else {
-	// build page
-	$page_data = elgg_view_layout('content', [
-		'title' => $title_text,
-		'content' => $content,
-		'filter' => '',
-	]);
-	
-	// draw page
-	echo elgg_view_page($title_text, $page_data);
+	return;
 }
+
+// full page
+echo elgg_view_page($title_text, [
+	'content' => $content,
+]);
